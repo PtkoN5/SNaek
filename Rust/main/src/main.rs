254c72ede0c8af8dbc2271425e8698ac0b46154d
Mika <mikaheining@gmail.com>
2024-12-21 09:03:18 +0100

    New rust implementation

--- Rust/main/src/main.rs
+++ Rust/main/src/main.rs
@@ -1,3 +1,28 @@
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new event loop
+    let event_loop = EventLoop::new();
+
+    // Create a new window
+    let window = WindowBuilder::new()
+        .with_title("SNaek")
+        .with_inner_size(winit::dpi::LogicalSize::new(800.0, 600.0))
+        .build(&event_loop)
+        .unwrap();
+
+    // Run the event loop
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait; // Wait for the events
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit, // Exit the event loop
+            _ => (),
+        }
+    });
 }
